d7aaf795f979aa4e8260c80db9af6b46e1d93d09
Mike Hodnick <mike@kindohm.com>
2019-03-26 18:21:47 -0500

    buzzing.

--- index.js
+++ index.js
@@ -7,43 +7,104 @@ var udpPort = new osc.UDPPort({
   metadata: true
 })
 
-// Listen for incoming OSC bundles.
-// udpPort.on('bundle', function(oscBundle, timeTag, info) {
-//   console.log(
-//     'An OSC bundle just arrived for time tag',
-//     timeTag,
-//     ':',
-//     oscBundle
-//   )
-//   console.log('Remote info is: ', info)
-// })
-
-// Open the socket.
 udpPort.open()
 
-// When the port is read, send an OSC message to, say, SuperCollider
+const maxFutureTime = 250
+const stepTime = 15
+const smallestTimeLeft = 50 // should be >= 40
+const server = '127.0.0.1'
+const port = 57120
+
+const steps = [
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'bd',
+  'cp',
+  '',
+  '',
+  '',
+  '',
+  ''
+]
+
+function shouldBatch(nextStartTime) {
+  const diff = nextStartTime - Date.now()
+  return diff <= smallestTimeLeft
+}
+
+function getBatch({ startStep, startTime, refTime }) {
+  const stepsToBatch = Math.ceil(stepTime / maxFutureTime)
+  let numStep = startStep
+  let messages = []
+
+  for (let i = 0; i < stepsToBatch; i++) {
+    if (steps[numStep] === '') continue
+    messages.push({
+      timeTag: osc.timeTag(
+        (startTime + i * stepTime - refTime) / 1000,
+        refTime
+      ),
+      packets: [
+        {
+          address: '/play2',
+          args: [
+            {
+              type: 's',
+              value: 's'
+            },
+            { type: 's', value: steps[numStep] }
+          ]
+        }
+      ]
+    })
+    numStep = numStep + 1 >= steps.length ? 0 : numStep + 1
+  }
+  return {
+    messages,
+    nextStartTime: startTime + stepsToBatch * stepTime,
+    nextStartStep:
+      startStep + stepsToBatch >= steps.length ? 0 : startStep + stepsToBatch
+  }
+}
+
+function sendMessages(messages) {
+  messages.forEach(message => udpPort.send(message, server, port))
+}
+
+let nextStartTime = null
+let currentStep = 0
+
+function loop() {
+  const now = Date.now()
+  nextStartTime = nextStartTime === null ? now + 200 : nextStartTime
+
+  setTimeout(() => {
+    if (shouldBatch(nextStartTime)) {
+      const batch = getBatch({
+        startStep: currentStep,
+        startTime: nextStartTime,
+        refTime: now
+      })
+
+      nextStartTime = batch.nextStartTime
+      currentStep = batch.nextStartStep
+
+      sendMessages(batch.messages)
+    } else {
+      //   console.log('nope', nextStartTime)
+    }
+    loop()
+  }, 10)
+}
+
 udpPort.on('ready', function() {
-  setInterval(() => {
-    udpPort.send(
-      {
-        timeTag: osc.timeTag(0.5),
-        packets: [
-          {
-            address: '/play2',
-            args: [
-              {
-                type: 's',
-                value: 's'
-              },
-              { type: 's', value: 'bd' },
-              { type: 's', value: 'cut' },
-              { type: 's', value: '1' }
-            ]
-          }
-        ]
-      },
-      '127.0.0.1',
-      57120
-    )
-  }, 500)
+  loop()
 })
